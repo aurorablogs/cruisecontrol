3705ee33812c827fbe7f31b68b95a5cd389406fe
aurorablogs <aurorablogs1@gmail.com>
2014-01-05 23:56:27 +0500

    Installer configurations
    Installer configurations

---
+++ Aurora_UI/TradeSimulator/WIX_Heat.bat
@@ -0,0 +1,3 @@
+heat dir "D:\Repos\cruisecontrol\Aurora_UI\Aurora_UI\bin\Release" -dr AuroraUI -cg AuroraUI -gg -g1 -sf -srd -sreg -var "var.UI" -out "D:\Repos\cruisecontrol\Aurora_UI\TradeSimulator\UI.wxs"
+
+heat dir "D:\Repos\cruisecontrol\Aurora_UI\DFsimulator\bin\Release" -dr Simulator -cg Simulator -gg -g1 -sf -srd -sreg -var "var.Sim" -out "D:\Repos\cruisecontrol\Aurora_UI\TradeSimulator\Simulator.wxs"
